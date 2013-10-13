48feac7c71d28df32ea415a12b3296b99fda23e5
MichaelFrizzell <MichaelFrizzell@fullsail.edu>
2013-10-12 20:28:27 -0400

    After_Video
    Document after video : Promts and Alerts.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-// JavaScript
+// JavaScript Prompts and Alerts
+
+var userYearOfBirth = prompt("Enter your year of birth:");
+console.log(userYearOfBirth);
+
+var width = prompt("Calculating the area of a rectangle. /nPlease enter the width");
+var height = prompt("Now enter the height");
+var area = width * height;
+//console.log(area);
+
+alert("The are of your rectangle is" + area);
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
